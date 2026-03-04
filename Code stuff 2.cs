0437267cf2046500db00c25ca3a820f87f215ac2
Jake M <0623025@schoolsnet.act.edu.au>
2026-03-05 09:03:55 +1100

    Update Code stuff 2.cs

--- Code stuff 2.cs
+++ Code stuff 2.cs
@@ -1,1 +1,24 @@
+Servo myservo;
 
+
+
+void setup()
+{
+myservo.attach(9);
+Serial.begin(9600);
+}
+
+void loop()
+{
+int deg = 20;
+while(deg <= 180)
+{
+delay(500);
+deg += 20;
+}
+deg = 160;
+while(deg >= 0){
+delay(500);
+deg -= 20;
+}
+} 
